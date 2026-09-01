f9fc8f0993dbabb3c6137f18bb84e50682bf6b80
nadesoares <nadesoaresfx@gmail.com>
2026-09-01 18:11:54 -0300

    Alterar senha de administrador para Pf@99947 no L2

--- Sistema de horário/js/store.js
+++ Sistema de horário/js/store.js
@@ -108,8 +108,8 @@ class Store {
       existingSchedules['2026-08'] = this.getDefaultAugust2026Schedule();
       localStorage.setItem(STORAGE_KEYS.SCHEDULES, JSON.stringify(existingSchedules));
     }
-    if (!localStorage.getItem(STORAGE_KEYS.ADMIN_PIN) || localStorage.getItem(STORAGE_KEYS.ADMIN_PIN) === '1234') {
-      localStorage.setItem(STORAGE_KEYS.ADMIN_PIN, 'Bd@9998');
+    if (!localStorage.getItem(STORAGE_KEYS.ADMIN_PIN) || localStorage.getItem(STORAGE_KEYS.ADMIN_PIN) === '1234' || localStorage.getItem(STORAGE_KEYS.ADMIN_PIN) === 'Bd@9998') {
+      localStorage.setItem(STORAGE_KEYS.ADMIN_PIN, 'Pf@99947');
     }
   }
 
@@ -276,10 +276,10 @@ class Store {
   }
 
   loginAdmin(pin) {
-    const savedPin = localStorage.getItem(STORAGE_KEYS.ADMIN_PIN) || 'Bd@9998';
-    if (pin === 'Bd@9998' || pin === savedPin) {
+    const savedPin = localStorage.getItem(STORAGE_KEYS.ADMIN_PIN) || 'Pf@99947';
+    if (pin === 'Pf@99947' || pin === savedPin) {
       sessionStorage.setItem(STORAGE_KEYS.ADMIN_SESSION, 'true');
-      localStorage.setItem(STORAGE_KEYS.ADMIN_PIN, 'Bd@9998');
+      localStorage.setItem(STORAGE_KEYS.ADMIN_PIN, 'Pf@99947');
       return true;
     }
     return false;
